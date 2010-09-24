f060cd4dce1fd80abfde998cebe7e270887f48eb
jhurliman <jhurliman@jhurliman.org>
2010-09-24 18:12:53 +0000

    Wiping out the SVN repository. The code has moved to github at http://github.com/openmetaversefoundation/

--- trunk/Simian/GridFrontend/application/views/user/search_results.php
+++
@@ -1,9 +0,0 @@
-<h2><?php echo count($user_list) . " " . lang('sg_results_found'); ?></h2>
-
-<ul>
-<?php
-    foreach ( $user_list as $user ) {
-        echo "<li>" . anchor("$site_url/user/view/" . $user['id'], $user['name'], array('class'=>'search_result')) . "</li>";
-    }
-?>
-</ul>
